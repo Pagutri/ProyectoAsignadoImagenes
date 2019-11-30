2cf643b1f1a5e975e182611b9ad22869b9771840
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-30 05:15:04 -0600

    :rocket: Tables ready.
    .

--- Pregunta1.py
+++ Pregunta1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[103]:
+# In[1]:
 
 
 # Type annotations :
@@ -60,6 +60,7 @@ from operator import itemgetter
 # In[2]:
 
 
+# Modules defined within this repo :
 import mfilt_funcs as mfs
 importlib.reload(mfs)
 import mfilt_funcs as mfs
@@ -91,8 +92,11 @@ plt.style.use('seaborn-deep')
 plt.rcParams['figure.figsize'] = (12, 8)
 
 
-# In[5]:
+# In[49]:
 
+
+
+#################################### Tested and functional : #######################################
 
 def chunk_pad(it, size, padval=None):
     """
@@ -103,6 +107,90 @@ def chunk_pad(it, size, padval=None):
     return iter(lambda: tuple(islice(it, size)), (padval,) * size)
 ##
 
+def segplot(
+    img: np.ndarray, 
+    group: skimage.measure._regionprops.RegionProperties, 
+    color: Optional[str] = None,
+    title: Optional[str] = None
+) -> NoReturn:
+    """
+    """
+    if not color:
+        color = 'red'
+        
+    fig, ax = plt.subplots(figsize=(9, 9))
+    ax.imshow(img, cmap='gray')
+
+    try:
+        iter(group)
+        for region in group:
+            minr, minc, maxr, maxc = region.bbox
+            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                      fill=False, edgecolor=color, linewidth=2)
+            ax.add_patch(rect)
+    except:
+        minr, minc, maxr, maxc = group.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                 fill=False, edgecolor=color, linewidth=2)
+        ax.add_patch(rect)
+        
+    
+    if title:
+        plt.title(title)
+    plt.tight_layout()
+    plt.show()
+##
+
+def watershed_viz(image, distance, labels):
+    """
+        Constructed from the example found in :
+        https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_watershed.html
+    """
+    fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
+    ax = axes.ravel()
+
+    ax[0].imshow(image, cmap=plt.cm.gray)
+    ax[0].set_title('Overlapping objects')
+    ax[1].imshow(-distance, cmap=plt.cm.gray)
+    ax[1].set_title('Distances')
+    ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
+    ax[2].set_title('Separated objects')
+
+    for a in ax:
+        a.set_axis_off()
+
+    fig.tight_layout()
+    plt.show()
+##
+
+def ez_watershed(
+    image: np.ndarray, 
+    markers: Optional[int] = None, 
+    footprint: Optional[np.array] = None, 
+    **kw
+) -> Tuple[int, int, int]:
+    """
+    """
+    distance = ndi.distance_transform_edt(image)
+    if footprint is not None:
+        fp = footprint
+    else:
+        fp = np.ones((10, 10))
+    
+    if markers is None:
+        local_maxi = peak_local_max(
+            distance, 
+            indices=False, 
+            footprint=np.ones((10, 10)),
+            labels=image,
+            **kw
+        )
+        markers = ndi.label(local_maxi)[0]
+
+    labels  = watershed(-distance, markers, mask=image)
+    
+    return markers, distance, labels
+##
 
 def auto_segment(
     img: np.ndarray, 
@@ -205,6 +293,128 @@ def ref_region(
         #plt.title('Lel')
         
     return _eroded
+##
+
+def subdivide_hose(
+    img: np.ndarray, 
+    n: int = 2, 
+    contiguous: bool = False,
+    disksize: Optional[float] = None
+) -> List[np.ndarray]:
+    """
+        Subdivide a hose into n chunks, automatically.
+    """
+    
+    _edges = canny(img)
+    _label_image = label(_edges, return_num=False)
+    _objs = regionprops(_label_image)
+    
+    _smallest = reduce(lambda x, y: x if x.area < y.area else y, _objs)
+    _largest  = reduce(lambda x, y: x if x.area > y.area else y, _objs)
+    
+    if contiguous:
+        # Sort according to columns. 
+        _short = np.array(sorted(_smallest.coords, key=itemgetter(1)))
+        _long  = np.array(sorted(_largest.coords,  key=itemgetter(1)))
+    else:
+        _short = _smallest.coords
+        _long  = _largest.coords
+    
+    _small_chunks = np.array_split(_short, n)
+    _large_chunks = np.array_split(_long, n)
+    
+    # Create n subdivision masks : 
+    _masked = [np.zeros_like(img, dtype=img.dtype) for i in range(n)]
+    
+    for i in range(len(_masked)):
+        for _coord in _small_chunks[i]:
+            _masked[i][tuple(_coord)] = 1
+        for _coord in _large_chunks[i]:
+            _masked[i][tuple(_coord)] = 1
+    
+    disksize = disksize if disksize is not None else 13
+    
+    return [ cv.bitwise_and(np.uint8(img), np.uint8(morphology.dilation(_mask, disk(disksize)))) for _mask in _masked ]
+##
+
+
+def plot_label_image_regions(img: np.ndarray) -> NoReturn:
+    """
+    """
+    # label image regions
+    label_image = label(img)
+    image_label_overlay = label2rgb(label_image, image=img)
+    font = cv.FONT_HERSHEY_SIMPLEX 
+    color = (255, 0, 0)
+    letrero = 1
+    thickness = 1
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    #ax.imshow(image_label_overlay)
+
+    for region in regionprops(label_image):
+        # draw rectangle around segmented labels
+        minr, minc, maxr, maxc = region.bbox
+        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                            fill=False, edgecolor='red', linewidth=2)
+        ax.add_patch(rect)
+        y0, x0 = region.centroid
+        y0 = int(y0)
+        x0 = int(x0)
+        org = (x0, y0)
+        image_label_overlay = cv.putText(image_label_overlay, str(letrero), org, font, 1, color, thickness, cv.LINE_AA)
+        letrero += 1
+    
+    ax.imshow(image_label_overlay)
+    ax.set_axis_off()
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+##
+
+#################################### Experimental : #######################################
+
+def my_thinning(img: np.ndarray, se: np.ndarray) -> np.ndarray:
+    """
+    """
+    
+    return  np.bitwise_xor(img, ndi.binary_hit_or_miss(img, se))
+
+
+def prune(img: np.ndarray, n: int = 1):
+    """
+        This function DOES NOT WORK !
+        HOW IS A DON'T CARE ELEMENT IMPLEMENTED IN PYTHON ?
+        :(
+    """
+     # Construct all of the structuring elements needed from clockwise rotations.
+    clockwise_rotations = lambda y: [y] + list(map(lambda x: np.rot90(y, x), reversed(range(1, 3+1))))
+    _b1 = np.array([[1, 0, 0],[1, 1, 0],[1, 0, 0]], dtype=img.dtype)
+    _b2 = np.array([[1, 0, 0],[0, 1, 0],[0, 0, 0]], dtype=img.dtype)
+    B = reduce(
+        lambda x, y: x + y, 
+        lmap(clockwise_rotations, [_b1, _b2])
+    )
+    H = np.ones((3, 3))
+    
+    # Thinning, by all of the structuring elements :
+    
+    X1 = reduce(thinning, B, img)
+    while n > 1:
+        X1 = reduce(thinning, B, X1)
+        n -= 1
+    
+    return X1
+    
+    # Hit or miss stage :
+    #X2 = reduce(ndi.binary_hit_or_miss, B, X1)
+
+    #return X2
+    
+    # Dilation stage : 
+    #X3 = cv.dilate(X2, H)
+    # Hit-or-miss
+    #ndi.binary_hit_or_miss  
 ##
 
 
@@ -441,7 +651,6 @@ segmented_ref_reg = {
 # In[28]:
 
 
-
 _tmp = copy.deepcopy(mangueras[llaves[0]][80:220, 210:350])
 #_tmp[ _tmp < 85] = 0
 #_tmp *= np.uint8( auto_segment(_tmp) * 255 )
@@ -502,14 +711,14 @@ region_info = pd.concat(region_info_list, axis=1)
 region_info.describe()
 
 
-# In[39]:
+# In[35]:
 
 
 # Relatively slow, avoid running :
 sns.pairplot(region_info.dropna())
 
 
-# In[49]:
+# In[36]:
 
 
 #find_branch_points(_hola).sum()
@@ -519,7 +728,7 @@ la_buena = reduce(cv.bitwise_xor, lmap(np.uint8, [sk, skl, ma, th]))
 utils.side_by_side(_tmp, la_buena)
 
 
-# In[142]:
+# In[37]:
 
 
 mangueras_segmentadas_amano = {
@@ -527,337 +736,171 @@ mangueras_segmentadas_amano = {
 }
 
 
-# In[143]:
+# In[69]:
 
 
-def subdivide_hose(
-    img: np.ndarray, 
-    n: int = 2, 
-    contiguous: bool = False,
-    disksize: Optional[float] = None
-) -> List[np.ndarray]:
+mascaras_agregadas_2 = {
+    key: reduce(cv.bitwise_xor, subdivide_hose(mangueras_segmentadas_amano[key], 2, contiguous=True)) for key in llaves
+}
+
+
+# In[70]:
+
+
+mascaras_subdivididas_2 = {
+    key: subdivide_hose(mangueras_segmentadas_amano[key], 2, contiguous=True) for key in llaves
+}
+
+
+# In[72]:
+
+
+_plot = False
+
+if _plot:
+    for llave in llaves:
+        plt.figure()
+        plt.imshow(mascaras_agregadas_2[llave], cmap='gray')
+
+
+# In[73]:
+
+
+mascaras_agregadas_6 = {
+    key: reduce(cv.bitwise_xor, subdivide_hose(mangueras_segmentadas_amano[key], 6, contiguous=False)) for key in llaves
+}
+
+
+# In[74]:
+
+
+mascaras_subdivididas_6 = {
+    key: subdivide_hose(mangueras_segmentadas_amano[key], 6, contiguous=False) for key in llaves
+}
+
+
+# In[75]:
+
+
+plot_label_image_regions(mascaras_agregadas_2[llaves[3]])
+plot_label_image_regions(mascaras_agregadas_6[llaves[3]])
+
+
+# In[76]:
+
+
+mangueras_subdivididas_2 = {
+    key: mangueras[key] * mascaras_subdivididas_2[key] for key in llaves
+}
+
+
+# In[84]:
+
+
+mangueras_agregadas_2 = {
+    key: mangueras[key] * mascaras_agregadas_2[key] for key in llaves
+}
+
+
+# In[85]:
+
+
+mangueras_agregadas_6 = {
+    key: mangueras[key] * mascaras_agregadas_6[key] for key in llaves
+}
+
+
+# In[86]:
+
+
+#plt.imshow(mangueras_subdivididas_2[llaves[0]][1])
+
+
+# In[87]:
+
+
+#plt.imshow(mangueras_subdivididas_2[llaves[0]], cmap='gray')
+
+
+# In[90]:
+
+
+mangueras_subdivididas_2[llaves[0]][0]
+
+
+# In[100]:
+
+
+
+
+
+# In[115]:
+
+
+def gen_tabla_subdivididas(y: Dict[str, List[np.ndarray]]) -> pd.core.frame.DataFrame:
     """
-        Subdivide a hose into n chunks, automatically.
-    """
-    
-    _edges = canny(img)
-    _label_image = label(_edges, return_num=False)
-    _objs = regionprops(_label_image)
-    
-    _smallest = reduce(lambda x, y: x if x.area < y.area else y, _objs)
-    _largest  = reduce(lambda x, y: x if x.area > y.area else y, _objs)
-    
-    if contiguous:
-        # Sort according to columns. 
-        _short = np.array(sorted(_smallest.coords, key=itemgetter(1)))
-        _long  = np.array(sorted(_largest.coords,  key=itemgetter(1)))
-    else:
-        _short = _smallest.coords
-        _long  = _largest.coords
-    
-    _small_chunks = np.array_split(_short, n)
-    _large_chunks = np.array_split(_long, n)
-    
-    # Create n subdivision masks : 
-    _masked = [np.zeros_like(img, dtype=img.dtype) for i in range(n)]
-    
-    for i in range(len(_masked)):
-        for _coord in _small_chunks[i]:
-            _masked[i][tuple(_coord)] = 1
-        for _coord in _large_chunks[i]:
-            _masked[i][tuple(_coord)] = 1
-    
-    disksize = disksize if disksize is not None else 13
-    
-    return [ cv.bitwise_and(np.uint8(img), np.uint8(morphology.dilation(_mask, disk(disksize)))) for _mask in _masked ]
-##    
-
-
-# In[144]:
-
-
-y = mangueras_segmentadas_amano[llaves[0]]
-yy = subdivide_hose(y, 6, contiguous=False, disksize=13)
-_agregated = reduce(cv.bitwise_xor, yy)
-plt.imshow(_agregated)
-#for sub in yy:
-    #utils.side_by_side(y, sub)
-
-
-# In[145]:
-
-
-def label_image_regions(img: np.ndarray) -> NoReturn:
-    """
-    """
-    # label image regions
-    label_image = label(_agregated)
-    image_label_overlay = label2rgb(label_image, image=_agregated)
-    font = cv.FONT_HERSHEY_SIMPLEX 
-    color = (255, 0, 0)
-    letrero = 1
-    thickness = 1
-
-    fig, ax = plt.subplots(figsize=(10, 6))
-    #ax.imshow(image_label_overlay)
-
-    for region in regionprops(label_image):
-        # draw rectangle around segmented labels
-        minr, minc, maxr, maxc = region.bbox
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                            fill=False, edgecolor='red', linewidth=2)
-        ax.add_patch(rect)
-        y0, x0 = region.centroid
-        y0 = int(y0)
-        x0 = int(x0)
-        org = (x0, y0)
-        image_label_overlay = cv.putText(image_label_overlay, str(letrero), org, font, 1, color, thickness, cv.LINE_AA)
-        letrero += 1
-    
-    ax.imshow(image_label_overlay)
-    ax.set_axis_off()
-    plt.tight_layout()
-    plt.show()
-    plt.close()
-##
-
-
-# In[146]:
-
-
-#help(mpatches)
-label_image_regions(_agregated)
-
-
-# In[267]:
-
-
-def get_neighbours(p, exclude_p=True, shape=None):
-    """
-        Snippet taken from :
-        https://stackoverflow.com/questions/34905274/how-to-find-the-neighbors-of-a-cell-in-an-ndarray
-        
-        Little example :
-        p = np.r_[4, 5]
-        shape = (6, 6)
-
-        neighbours = get_neighbours(p, shape=shape)
-
-        x = np.zeros(shape, int)
-        x[tuple(neighbours.T)] = 1
-        x[tuple(p)] = 2
-
-        print(x)
-        # [[0 0 0 0 0 0]
-        #  [0 0 0 0 0 0]
-        #  [0 0 0 0 0 0]
-        #  [0 0 0 0 1 1]
-        #  [0 0 0 0 1 2]
-        #  [0 0 0 0 1 1]]
-    """
-    ndim = len(p)
-
-    # generate an (m, ndims) array containing all strings over the alphabet {0, 1, 2}:
-    offset_idx = np.indices((3,) * ndim).reshape(ndim, -1).T
-
-    # use these to index into np.array([-1, 0, 1]) to get offsets
-    offsets = np.r_[-1, 0, 1].take(offset_idx)
-
-    # optional: exclude offsets of 0, 0, ..., 0 (i.e. p itself)
-    if exclude_p:
-        offsets = offsets[np.any(offsets, 1)]
-
-    neighbours = p + offsets    # apply offsets to p
-
-    # optional: exclude out-of-bounds indices
-    if shape is not None:
-        valid = np.all((neighbours < np.array(shape)) & (neighbours >= 0), axis=1)
-        neighbours = neighbours[valid]
-
-    return neighbours
-
-
-# In[305]:
-
-
-#%%timeit
-split_nodes: list = []
-    
-for point in el_obj.coords:
-    _neighbours = 0
-    for nei in get_neighbours(point, shape=preg4.shape):
-        _neighbours += preg4[tuple(nei)]
-    if _neighbours > 2:
-        split_nodes.append(point)
-
-print(split_nodes)
-
-
-# In[308]:
-
-
-for point in split_nodes:
-    print(point)
-    for nei in get_neighbours(point, shape=preg4.shape):
-        print('\t', nei, preg4[tuple(nei)])
-    print('\n')
-
-
-# In[318]:
-
-
-path_length: Dict[np.ndarray, int] = {}
-visited: List[np.ndarray] = [] 
-
-
-for point in split_nodes:
-    print(point)
-    visited.append(point)
-    _neighbours = (lambda y: [x for x in get_neighbours(point, shape=y.shape) if y[tuple(x)]])(preg4)
-    visited += _neighbours
-    for nei in _neighbours:
-        print('\t', nei, preg4[tuple(nei)])
-    print('\n')
-
-
-# In[340]:
-
-
-def thinning(img: np.ndarray, se: np.ndarray) -> np.ndarray:
-    """
+        Generate dianostic tables.
     """
     
-    return  np.bitwise_xor(img, ndi.binary_hit_or_miss(img, se))
+    return pd.core.frame.DataFrame({
+        "Num. pixeles": [len(x[ x != 0]) for x in y ],
+        "Intensidad media": [ x[ x != 0].mean() for x in y ],
+        "Varianza": [ x[ x != 0].std()**2 for x in y ]
+    })
 
 
-def prune(img: np.ndarray, n: int = 1):
-    """
-    """
-    
-     # Construct all of the structuring elements needed from clockwise rotations.
-    clockwise_rotations = lambda y: [y] + list(map(lambda x: np.rot90(y, x), reversed(range(1, 3+1))))
-    _b1 = np.array([[1, 0, 0],[1, 1, 0],[1, 0, 0]], dtype=img.dtype)
-    _b2 = np.array([[1, 0, 0],[0, 1, 0],[0, 0, 0]], dtype=img.dtype)
-    B = reduce(
-        lambda x, y: x + y, 
-        lmap(clockwise_rotations, [_b1, _b2])
-    )
-    H = np.ones((3, 3))
-    
-    # Thinning, by all of the structuring elements :
-    
-    X1 = reduce(thinning, B, img)
-    while n > 1:
-        X1 = reduce(thinning, B, X1)
-        n -= 1
-    
-    return X1
-    
-    # Hit or miss stage :
-    #X2 = reduce(ndi.binary_hit_or_miss, B, X1)
-
-    #return X2
-    
-    # Dilation stage : 
-    #X3 = cv.dilate(X2, H)
-    # Hit-or-miss
-    #ndi.binary_hit_or_miss
-    
-    
-##
+# In[116]:
 
 
-# In[342]:
+y = mangueras_subdivididas_2[llaves[0]]
+[type(x) for x in y]
 
 
-neighbours = lambda image, pos: pad1( image[max(0, pos[0]-1):min(pos[0]+1, image.shape[0]), max(0, pos[1]-1):min(pos[1]+1, image.shape[1])]  )
+# In[117]:
 
 
-# In[45]:
+tabla_2_secciones = {
+    key: gen_tabla_subdivididas(value) for key, value in mangueras_subdivididas_2.items()
+}
 
 
-def segplot(
-    img: np.ndarray, 
-    group: skimage.measure._regionprops.RegionProperties, 
-    color: Optional[str] = None,
-    title: Optional[str] = None
-) -> NoReturn:
-    """
-    """
-    if not color:
-        color = 'red'
-        
-    fig, ax = plt.subplots(figsize=(9, 9))
-    ax.imshow(img, cmap='gray')
+# In[120]:
 
-    try:
-        iter(group)
-        for region in group:
-            minr, minc, maxr, maxc = region.bbox
-            rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                      fill=False, edgecolor=color, linewidth=2)
-            ax.add_patch(rect)
-    except:
-        minr, minc, maxr, maxc = group.bbox
-        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                 fill=False, edgecolor=color, linewidth=2)
-        ax.add_patch(rect)
-        
-    
-    if title:
-        plt.title(title)
-    plt.tight_layout()
-    plt.show()
-##
 
-def watershed_viz(image, distance, labels):
-    """
-        Constructed from the example found in :
-        https://scikit-image.org/docs/dev/auto_examples/segmentation/plot_watershed.html
-    """
-    fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
-    ax = axes.ravel()
+tabla_6_secciones = {
+    key: gen_tabla_subdivididas(value) for key, value in mangueras_subdivididas_6.items()
+}
 
-    ax[0].imshow(image, cmap=plt.cm.gray)
-    ax[0].set_title('Overlapping objects')
-    ax[1].imshow(-distance, cmap=plt.cm.gray)
-    ax[1].set_title('Distances')
-    ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
-    ax[2].set_title('Separated objects')
 
-    for a in ax:
-        a.set_axis_off()
+# In[122]:
 
-    fig.tight_layout()
-    plt.show()
-##
 
-def ez_watershed(
-    image: np.ndarray, 
-    markers: Optional[int] = None, 
-    footprint: Optional[np.array] = None, 
-    **kw
-) -> Tuple[int, int, int]:
-    """
-    """
-    distance = ndi.distance_transform_edt(image)
-    if footprint is not None:
-        fp = footprint
-    else:
-        fp = np.ones((10, 10))
-    
-    if markers is None:
-        local_maxi = peak_local_max(
-            distance, 
-            indices=False, 
-            footprint=np.ones((10, 10)),
-            labels=image,
-            **kw
-        )
-        markers = ndi.label(local_maxi)[0]
+#print(tabla_2_secciones[llaves[1]])
+tabla_6_secciones[llaves[1]]
 
-    labels  = watershed(-distance, markers, mask=image)
-    
-    return markers, distance, labels
-##
+
+# In[119]:
+
+
+_sec1 = mangueras_subdivididas_2[llaves[1]][0]
+_data1 = _sec1[ _sec1 != 0].flatten()
+print(_data1.mean(), _data1.std()**2)
+sns.distplot(_data1)
+
+
+# In[68]:
+
+
+_plot = False
+
+if _plot:
+    for llave in llaves:
+        plt.figure()
+        plt.imshow(mangueras_subdivididas_6[llave], cmap='gray')
+
+
+# In[ ]:
+
+
+
 
